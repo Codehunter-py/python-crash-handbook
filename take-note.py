0d09a531da9fc06512cfcc46cabfbcf10bcd409f
Codehunter-py <ibrahim.musayev2021@outlook.com>
2021-11-02 23:01:01 +0100

    building basic take-note app

---
+++ take-note.py
@@ -0,0 +1,22 @@
+""" Chapet X """
+
+import os
+os.chdir('c:/Users/Ibrahim/Desktop/python-crash-handbook')
+
+print("=== Press 'q' to quit ===")
+print("=== Press 's' to display note ===\n")
+file = "programming.txt"
+active=True
+text=''
+while active:
+    text=input("Your Note goes here: ")
+    with open(file, 'a') as file_object: #append and 'w' write
+        file_object.write(f"\n{text}")
+
+    if text == 's':
+        with open(file) as f:
+            contents = f.read()
+            print(contents)
+
+    if text == 'q':
+        active = False
