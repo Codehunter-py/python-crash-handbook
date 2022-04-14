33d6bd5bba72585a957d39ecf283754dc50c6486
Ibrahim Musayev <84103775+Codehunter-py@users.noreply.github.com>
2022-04-14 16:48:20 +0200

    booking app

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+   "fmt"
+   "strings"
+)
+
+
+func main() {
+   conferenceName := "Go Conference"
+   const conferenceTickets int = 50
+   var remainingTickets uint = 50
+   var bookings []string
+
+   fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceName, conferenceTickets, remainingTickets)
+   
+   fmt.Printf("Welcome to %v booking application\n", conferenceName)
+   fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+   fmt.Println("Get your tickets here to attend")
+   
+   for {
+      var firstName string
+      var lastName string
+      var email string
+      var userTickets uint 
+      // ask for input 
+      fmt.Println("Enter your first name: ")
+      fmt.Scan(&firstName)
+   
+      fmt.Println("Enter your surname: ")
+      fmt.Scan(&lastName)   
+   
+      fmt.Println("Enter your email address: ")
+      fmt.Scan(&email)
+    
+      fmt.Println("Enter your number of tickets: ")
+      fmt.Scan(&userTickets)
+      
+      if userTickets > remainingTickets {
+         fmt.Printf("We only have %v tickets remaining, you can't book %v tickets\n", remainingTickets, userTickets)
+         break
+      }
+
+      remainingTickets = remainingTickets - userTickets
+      bookings = append(bookings, firstName + " " + lastName)
+   
+      // fmt.Printf("The whole slice: %v\n", bookings)
+      // fmt.Printf("The first value: %v\n", bookings[0])
+      // fmt.Printf("slice type: %T\n", bookings)
+      // fmt.Printf("slice length: %v\n", len(bookings))
+      
+      fmt.Printf("thanks %v %v for booking %v tickets. You will receive confirmation at %v\n", firstName, lastName, userTickets, email)
+      fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+      
+
+      firstNames := []string{}
+      for _, booking := range bookings {
+         var names = strings.Fields(booking)
+         firstNames = append(firstNames, names[0])
+      }
+      fmt.Printf("These firstnames of total bookings in our app: %v\n", firstNames)
+   
+      noTicketsRemaining := remainingTickets == 0 // alternatively, var noTicketsRemaining bool = remainingTickets == 0 
+      if noTicketsRemaining {
+         // end program 
+         fmt.Println("Our conference is booked out.")
+         break
+      }
+   }
+} // conditionals 
