9e6126f972d56ef7cdc13df55f274814408855e7
Codehunter-py <ibrahim.musayev2021@outlook.com>
2022-04-15 14:14:15 +0200

    adapt decision tree

--- booking-app/main.go
+++ booking-app/main.go
@@ -19,7 +19,7 @@ func main() {
    fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
    fmt.Println("Get your tickets here to attend")
    
-   for {
+   for { // loop until all tickets are booked or no more tickets are available
       var firstName string
       var lastName string
       var email string
@@ -36,36 +36,50 @@ func main() {
     
       fmt.Println("Enter your number of tickets: ")
       fmt.Scan(&userTickets)
-      
-      if userTickets > remainingTickets {
-         fmt.Printf("We only have %v tickets remaining, you can't book %v tickets\n", remainingTickets, userTickets)
-         break
-      }
 
-      remainingTickets = remainingTickets - userTickets
-      bookings = append(bookings, firstName + " " + lastName)
-   
-      // fmt.Printf("The whole slice: %v\n", bookings)
-      // fmt.Printf("The first value: %v\n", bookings[0])
-      // fmt.Printf("slice type: %T\n", bookings)
-      // fmt.Printf("slice length: %v\n", len(bookings))
-      
-      fmt.Printf("thanks %v %v for booking %v tickets. You will receive confirmation at %v\n", firstName, lastName, userTickets, email)
-      fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+      // check valid user input
+      isValidName := len(firstName) >=2 && len(lastName) >=2
+      isValidEmail := strings.Contains(email, "@") && strings.Contains(email, ".")
+      isValidTickets := userTickets > 0 && userTickets <= remainingTickets
       
 
-      firstNames := []string{}
-      for _, booking := range bookings {
-         var names = strings.Fields(booking)
-         firstNames = append(firstNames, names[0])
-      }
-      fmt.Printf("These firstnames of total bookings in our app: %v\n", firstNames)
+      if  isValidName && isValidEmail && isValidTickets {
+
+         remainingTickets = remainingTickets - userTickets
+         bookings = append(bookings, firstName + " " + lastName)
+      
+         // fmt.Printf("The whole slice: %v\n", bookings)
+         // fmt.Printf("The first value: %v\n", bookings[0])
+         // fmt.Printf("slice type: %T\n", bookings)
+         // fmt.Printf("slice length: %v\n", len(bookings))
+         
+         fmt.Printf("thanks %v %v for booking %v tickets. You will receive confirmation at %v\n", firstName, lastName, userTickets, email)
+         fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+         
    
-      noTicketsRemaining := remainingTickets == 0 // alternatively, var noTicketsRemaining bool = remainingTickets == 0 
-      if noTicketsRemaining {
-         // end program 
-         fmt.Println("Our conference is booked out.")
-         break
+         firstNames := []string{} // define slice of strings
+         for _, booking := range bookings {
+            var names = strings.Fields(booking)
+            firstNames = append(firstNames, names[0])
+         }
+         fmt.Printf("These firstnames of total bookings in our app: %v\n", firstNames)
+      
+         noTicketsRemaining := remainingTickets == 0 // alternatively, var noTicketsRemaining bool = remainingTickets == 0 
+         if noTicketsRemaining {
+            // end program 
+            fmt.Println("Our conference is booked out.")
+            break
+         }
+      } else {
+         if !isValidName {
+            fmt.Println("Firstname and lastname must be at least 2 characters long.")
+         }
+         if !isValidEmail {
+            fmt.Println("Email address is invalid.")
+         }
+         if !isValidTickets {
+            fmt.Println("Number of tickets must be greater than 0 and less than or equal to remaining tickets.")
+         }
       }
    }
 } // conditionals 
